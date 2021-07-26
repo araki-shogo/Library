63b11f6fbd8603fdf9acf9ea58aa351e251483a6
araki_shogo <araki@crane-inc.io>
2021-07-26 11:27:37 +0900

    Change views

--- Library/resources/views/books/edit.blade.php
+++ Library/resources/views/books/edit.blade.php
@@ -11,6 +11,16 @@
     @method('delete')
     <input type="submit" value="削除">
 </form>
+<form action="/lendings/lent" method="post">
+    @csrf
+    <input name="book_id" type="hidden" value="{{ $datas->id }}">
+    <input type="submit" value="借りる">
+</form>
+<form action="/lendings/return" method="post">
+    @csrf
+    <input name="book_id" type="hidden" value="{{ $datas->id }}">
+    <input type="submit" value="返却する">
+</form>
 
 
 @endsection
--- Library/resources/views/books/index.blade.php
+++ Library/resources/views/books/index.blade.php
@@ -4,16 +4,6 @@
     @foreach($datas as $data)
         <div style="display: flex;">
             <a href="/books/edit/{{$data->id}}" >id：{{ $data->id }}　タイトル：{{ $data->title }}</a>
-            <form action="/lendings/lent" method="post">
-                @csrf
-                <input name="book_id" type="hidden" value="{{ $data->id }}">
-                <input type="submit" value="借りる">
-            </form>
-            <form action="/lendings/return" method="post">
-                @csrf
-                <input name="book_id" type="hidden" value="{{ $data->id }}">
-                <input type="submit" value="返却する">
-            </form>
         </div>
     @endforeach
 @endsection
