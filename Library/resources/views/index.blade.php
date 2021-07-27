92523d6da66be873144b5dde581d84b82cb16334
araki_shogo <araki@crane-inc.io>
2021-07-27 20:05:15 +0900

    日付の表記変更

--- Library/resources/views/index.blade.php
+++ Library/resources/views/index.blade.php
@@ -7,7 +7,7 @@
         id：{{ $data->id }}
         タイトル：{{ $data->book->title }}
         ユーザー：{{ $data->user->name }}
-        貸出日：{{ $data->lent_date }}
+        貸出日：{{ date('Y年n月j日', strtotime($data->lent_date)) }}
         <br>
     @endforeach
 @endsection
--- Library/resources/views/lendings/index.blade.php
+++ Library/resources/views/lendings/index.blade.php
@@ -7,8 +7,11 @@
         id：{{ $data->id }}
         タイトル：{{ $data->book->title }}
         本：{{ $data->user->name }}
-        貸出日：{{ $data->lent_date }}
-        返却日：{{ $data->return_date }}
+        貸出日：{{ date('Y-m-j', strtotime($data->lent_date)) }}
+        返却日：
+        @if(date('Y-m-j', strtotime($data->return_date)) != '1970-01-1') 
+            {{ date('Y-m-j', strtotime($data->return_date)) }}
+        @endif
         <br>
     @endforeach
 @endsection
